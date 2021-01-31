5806d17584614adc60125f20028efe927964699f
Edson Lucas <EDSONLUCASBD94@GMAIL.COM>
2021-01-30 23:06:30 -0300

    Delete ReactToDos directory

--- ReactToDos/frontend/src/components/ButtonContainer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import css from '../css/styles.module.css';
-
-export default function ButtonContainer({children}) {
-  return <div className={css.flexRow}>{children}</div>;
-}
-
